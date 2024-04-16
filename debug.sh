bb7409159cc7495361eaf2231fcb8006aecfb254
Yatao Li <yatli@microsoft.com>
2024-04-16 23:30:35 +0800

    update debug

--- debug.sh
+++ debug.sh
@@ -4,12 +4,27 @@ export KERNEL=kernel8
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
+UPDATE_KERNEL=0
+UPDATE_DTBS=1
+
 make -j16 Image modules dtbs
 INSTALL_MOD_PATH=../modules make modules_install -j16
+
+if [[ "$UPDATE_DTBS" != "" ]]
+then
+  rm -rf ../out/*
+  mkdir -p ../out/overlays
+
+  cp arch/arm64/boot/dts/broadcom/*.dtb ../out
+  cp arch/arm64/boot/dts/overlays/*.dtb* ../out/overlays/
+  cp arch/arm64/boot/dts/overlays/README ../out/overlays/
+  cp arch/arm64/boot/Image ../out/$KERNEL.img
+  scp -r ../out uconsole:~/
+  ssh uConsole "sudo mv out/* /boot/"
+  ssh uConsole "sudo mv out/overlays /boot/overlays/"
+fi
+
 cp arch/arm64/boot/Image ../out/$KERNEL.img
-
-#UPDATE_KERNEL=1
-
 scp ../modules/lib/modules/6.1.21-v8+/kernel/drivers/gpu/drm/vc4/vc4.ko.xz 192.168.50.4:~/
 scp ../modules/lib/modules/6.1.21-v8+/kernel/drivers/gpu/drm/panel/panel-cwu50.ko.xz 192.168.50.4:~/
 scp ../modules/lib/modules/6.1.21-v8+/kernel/drivers/video/backlight/ocp8178_bl.ko.xz 192.168.50.4:~/
